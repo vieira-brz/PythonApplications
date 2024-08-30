93ed7b1027b1d756ce6bcdfdcf1b0f871c931568
vieira-brz <vieirabraz2003@gmail.com>
2024-08-30 16:56:39 -0300

    Email - Outlook (adding comments)

--- Emails/outlook.py
+++ Emails/outlook.py
@@ -1,13 +1,21 @@
 import win32com.client as win32
 
+# Inicializa a aplicação do Outlook
 outlook = win32.Dispatch('outlook.application')
 
+# Cria um novo e-mail
 email = outlook.CreateItem(0)
 
+# Define o nome que será exibido no corpo do e-mail
 my_name = 'Código Python'
 
+# Configura o destinatário do e-mail
 email.To = "someone@gmail.com; someone2@gmail.com"
+
+# Define o assunto do e-mail
 email.Subject = "E-mail automático do Python"
+
+# Cria o corpo do e-mail em formato HTML
 email.HTMLBody = f"""
     <p>Olá, este é o {my_name}!</p>
     <p>Seja bem vindo ao seu novo ambiente de desenvolvimento.</p>
@@ -15,7 +23,9 @@ email.HTMLBody = f"""
     <p>{my_name}</p>
 """
 
+# Adiciona um anexo ao e-mail
 anexo = "C://Users/user/Downloads/arquivo.xslx"
 email.Attachments.Add(anexo)
 
+# Envia o e-mail
 email.Send()
